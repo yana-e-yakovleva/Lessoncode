e8268d8a8261a4d73efff489b846a988d0f1d988
Yana Yakovleva <yana.e.yakovleva@yandex.ru>
2022-11-24 16:59:29 +0300

    Task2

---
+++ Lesson2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA == numberB * numberB)
+{    
+Console.WriteLine("yes");
+}
+
+else {
+Console.WriteLine("no");
+}
+
