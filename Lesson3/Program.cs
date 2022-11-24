83be476d038c7816459442c0d4213460f231fffc
Yana Yakovleva <yana.e.yakovleva@yandex.ru>
2022-11-24 17:39:13 +0300

    Lesson3

---
+++ Lesson3/Program.cs
@@ -0,0 +1,24 @@
+﻿int number = int.Parse(Console.ReadLine());
+string day = Console.ReadLine();
+
+if (number == 1)
+ {Console.WriteLine("Monday");}
+
+if (number == 2)
+ {Console.WriteLine("Tuesday");}
+
+if (number == 3)
+{ Console.WriteLine("Wednesday");}
+
+if (number == 4)
+ {Console.WriteLine("Thursday");}
+
+if (number == 5)
+ {Console.WriteLine("Friday");}
+
+if (number == 6)
+ {Console.WriteLine("Saturday");}
+
+if (number == 7)
+ {Console.WriteLine("Sunday");}
+
